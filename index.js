0e120c4a09ae20b62e61710c0d7a8c9c91511f83
masud309060 <masud306090@gmail.com>
2020-10-05 21:46:56 +0600

    halka problem done pass user

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ require('dotenv').config()
 const { ObjectId } = require('mongodb');
 
 
-const uri = `mongodb+srv://${process.env.DB_PASS}:${process.env.DB_USER}@cluster0.rrq7z.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rrq7z.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
-
 
 client.connect(err => {
   const eventsCollection = client.db(`${process.env.DB_NAME}`).collection("events");
